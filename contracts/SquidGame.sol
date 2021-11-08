e2b744fd67191e8081b3b41199cf4a57e699f4b0
katie <okdonga@gmail.com>
2021-11-09 00:48:19 +0900

    Allow multiple ownership of NFTs

--- contracts/SquidGame.sol
+++ contracts/SquidGame.sol
@@ -1,19 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-// NFT contract to inherit from.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-
 import "./libraries/Base64.sol";
-
 import "hardhat/console.sol";
 
-contract SquidGame is ERC721 {
-    // A struct holds all the character's attributes 
+contract SquidGame is ERC721Enumerable, Ownable {
     struct CharacterAttributes {
         uint256 characterIndex;
         string name;
@@ -23,8 +19,6 @@ contract SquidGame is ERC721 {
         uint256 attackDamage;
     }
 
-    // The tokenId is the NFTs unique identifier, it's just a number that goes
-    // 0, 1, 2, 3, etc.
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -32,13 +26,13 @@ contract SquidGame is ERC721 {
 
     // tokenId => NFT attributes mapping 
     mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
-    // address => NFT tokenId : Q: what if the address owns multiple NFTs ? 
-    mapping(address => uint256) public nftHolders;
+
+    // Mapping from token ID to owner address (owner can have multiple NFTs)
+    mapping(uint256 => address) private _owners;
 
     event CharacterNFTMinted(address sender, uint256 tokenId, uint256 characterIndex);
     event AttackComplete(uint256 newBossHp, uint256 newPlayerHp);
 
-    // This will live on smart contract (not an NFT) - this is our enemy to attack
     struct BigBoss {
         string name;
         string imageURI;
@@ -62,8 +56,6 @@ contract SquidGame is ERC721 {
     ) 
         ERC721("SquidGame", "SQU")
     {
-        console.log("Deploying a SquidGame contract");
-        // Initialize the boss. 
         bigBoss = BigBoss({
             name: bossName,
             imageURI: bossImageURI,
@@ -71,7 +63,6 @@ contract SquidGame is ERC721 {
             maxHp: bossHp,
             attackDamage: bossAttackDamage
         });
-        console.log("Done initializing boss %s w/ HP %s, img %s", bigBoss.name, bigBoss.hp, bigBoss.imageURI);
 
         for (uint256 i = 0; i < characterNames.length; i++) {
             defaultCharacters.push(
@@ -85,22 +76,17 @@ contract SquidGame is ERC721 {
                 })
             );
 
-            CharacterAttributes memory c = defaultCharacters[i];
-
-            // Hardhat's use of console.log() allows up to 4 parameters in any order of following types: uint, string, bool, address
-            console.log("Done initializing %s w/ HP %s, igmg %s", c.name, c.hp, c.imageURI);
+            // CharacterAttributes memory c = defaultCharacters[i];
+            // console.log("Done initializing %s w/ HP %s, igmg %s", c.name, c.hp, c.imageURI);
         }
 
         _tokenIds.increment();
     }
 
-    // Users can get their NFT based on the characterId they send in 
     function mintCharacterNFT(uint256 _characterIndex) external {
         // Starts at 1 since it's being incremented in the constructor
         uint256 newItemId = _tokenIds.current();
 
-        // Assign the tokenId to the caller's wallet address 
-        // By using msg.sender you can't "fake" someone else's public address unless
         _safeMint(msg.sender, newItemId);
 
         require(_characterIndex < defaultCharacters.length, "Not a valid index");
@@ -115,7 +101,7 @@ contract SquidGame is ERC721 {
 
         console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
 
-        nftHolders[msg.sender] = newItemId;
+        _owners[newItemId] = msg.sender;
 
         _tokenIds.increment();
 
@@ -125,12 +111,10 @@ contract SquidGame is ERC721 {
     function tokenURI(uint256 _tokenId) public view override returns (string memory) {
         CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
 
-        // convert uint256 into string
         string memory strHp = Strings.toString(charAttributes.hp);
         string memory strMaxHp = Strings.toString(charAttributes.maxHp);
         string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
 
-        // abi.encodePacked just combines strings.
         string memory json = Base64.encode(
             bytes(
                 string(
@@ -155,13 +139,14 @@ contract SquidGame is ERC721 {
         return output;
     }
 
-    function attackBoss() public {
-        console.log("ATTACK BOSS!");
-        uint256 tokenIdOfPlayer = nftHolders[msg.sender];
-        console.log(tokenIdOfPlayer);
+    function attackBoss(uint256 tokenId) public {
+        uint256 tokenIdOfPlayer = tokenId;
         CharacterAttributes storage player = nftHolderAttributes[tokenIdOfPlayer];
-        console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
-        console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+        // console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
+        // console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+        
+        // First, check if you own the tokenId 
+        require(_owners[tokenId] == msg.sender);
         // Make sure the player has more than 0 HP.
         require(player.hp > 0, "Player doesn't have enough HP");
         // Make sure the boss has more than 0 HP.
@@ -180,20 +165,14 @@ contract SquidGame is ERC721 {
             player.hp = player.hp - bigBoss.attackDamage;
         }
 
-        console.log("Boss attacked player. New player hp: %s\n", player.hp);
-        console.log("Player attacked boss. Boss hp: %s\n", bigBoss.hp);
+        // console.log("Boss attacked player. New player hp: %s\n", player.hp);
+        // console.log("Player attacked boss. Boss hp: %s\n", bigBoss.hp);
 
         emit AttackComplete(bigBoss.hp, player.hp);
     }
 
-    function checkIfUserHasNFT() public view returns (CharacterAttributes memory) {
-        uint256 tokenId = nftHolders[msg.sender];
-        if (tokenId == 0) {
-            // User has no NFT, return an empty character 
-            CharacterAttributes memory emptyStruct;
-            return emptyStruct; 
-        } 
-        return nftHolderAttributes[tokenId];
+    function getCharacterAttributes(uint256 _tokenId) public view returns (CharacterAttributes memory) {
+        return nftHolderAttributes[_tokenId];
     }
 
     function getAllDefaultCharacters() public view returns (CharacterAttributes[] memory) {
@@ -202,5 +181,34 @@ contract SquidGame is ERC721 {
 
     function getBigBoss() public view returns (BigBoss memory) {
         return bigBoss;
+    }
+
+    /**
+     * Returns a list of tokens owned by _owner
+     */
+    function tokensOfOwner(address _owner) external view returns(uint256[] memory ownerTokens) {
+        uint256 tokenCount = balanceOf(_owner);
+
+        if (tokenCount == 0) {
+            // Return an empty array
+            return new uint256[](0);
+        } else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 totalNFTs = totalSupply();
+            uint256 resultIndex = 0;
+
+            // We count on the fact that all NFTs have IDs starting at 1 and increasing
+            // sequentially up to the totalNFTs count.
+            uint256 nftId;
+
+            for (nftId = 1; nftId <= totalNFTs; nftId++) {
+                if (_owners[nftId] == _owner) {
+                    result[resultIndex] = nftId;
+                    resultIndex++;
+                }
+            }
+
+            return result;
+        }
     }
 }
